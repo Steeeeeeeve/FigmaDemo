6080f223fa3c76caffca0643535b844956026022
Steeeeeeeve <steven.hurkett@gmail.com>
2023-12-04 12:04:28 -0600

    updated relay import

--- app/src/main/java/com/example/figmacoopt/MainActivity.kt
+++ app/src/main/java/com/example/figmacoopt/MainActivity.kt
@@ -7,42 +7,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.figmacoopt.loginbutton.LoginButton
 import com.example.figmacoopt.ui.theme.FigmaCoOptTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FigmaCoOptTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            button()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Column{
-    }
+fun button() {
+    LoginButton(title = "Login")
 }
